aeb305b2decbf06d9eb1f05b207cad6aa00c1a0e
bet630 <vanik9411@gmail.com>
2025-08-16 16:51:18 +0530

    Spring Boot Project

---
+++ ManytoManyMapping/src/main/java/com/tns/many_to_many_mapping/ManytoManyMappingApplication.java
@@ -0,0 +1,13 @@
+package com.tns.many_to_many_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytoManyMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytoManyMappingApplication.class, args);
+	}
+
+}
---
+++ ManytoManyMapping/src/test/java/com/tns/many_to_many_mapping/ManytoManyMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.many_to_many_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytoManyMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

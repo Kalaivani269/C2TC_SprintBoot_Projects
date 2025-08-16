d1ecb2c940e297114282e6907f137f5626571056
bet630 <vanik9411@gmail.com>
2025-08-16 16:52:07 +0530

    Spring Boot Project

---
+++ OnetoManyMapping/src/main/java/com/tns/one_to_many_mapping/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+
+package com.tns.one_to_many_mapping.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+    // Constructor accepting a custom message
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}

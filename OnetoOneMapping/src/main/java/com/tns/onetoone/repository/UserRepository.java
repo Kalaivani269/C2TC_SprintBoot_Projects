271f90b8557c05d34ab32388268533cba074a231
bet630 <vanik9411@gmail.com>
2025-08-16 16:53:04 +0530

    Spring Boot Project

---
+++ OnetoOneMapping/src/main/java/com/tns/onetoone/repository/UserRepository.java
@@ -0,0 +1,10 @@
+
+package com.tns.onetoone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tns.onetoone.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
